de3d708d5684c1a4d61c324221e49de1c9c4762d
ilcosme <fer.c@live.com.ar>
2022-03-11 18:43:19 -0300

    intentoCalculadoraAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,87 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora-app';
+  operandoa:number = 0;
+  operandob:number = 0;
+  resultado:string = "";
+  opera:string = "";
+
+  suma():void{
+    this.operandoa = parseInt(this.resultado)
+    this.opera="+";
+    this.limpiar();
+  }
+
+  resta():void{
+    this.operandoa = parseInt(this.resultado);
+    this.opera="-";
+    this.limpiar();
+  }
+
+  multiplicacion():void{
+    this.operandoa = parseInt(this.resultado);
+    this.opera="*";
+    this.limpiar();
+  }
+
+  division():void{
+    this.operandoa = parseInt(this.resultado);
+    this.opera="/";
+    this.limpiar();
+  }
+
+  igual():void{
+    this.operandoa = parseInt(this.resultado);
+    this.resolver();
+  }
+
+  resolver():void{
+    let res=0;
+
+    if(this.opera="+"){
+     res = this.operandoa + this.operandob;
+    } else if(this.opera="-"){
+      res = this.operandoa - this.operandob;
+    } else if(this.opera="*"){
+      res = this.operandoa * this.operandob;
+    } else if(this.opera="/"){
+      res = this.operandoa / this.operandob;
+    }
+
+    this.resetear();
+    res = parseInt(this.resultado);
+    console.log(res);
+
+
+  }
+
+  limpiar():void{
+    this.resultado="";
+  }
+
+  resetear():void{
+    this.operandoa = 0;
+    this.operandob = 0;
+    this.opera = "";
+    this.resultado = "";
+  }
+
+ /* onSumar():void{
+    this.operandoa=+this.resultado;
+    this.operacion = this.operandoa + this.operandob;
+  }
+
+  onRestar():void{
+    this.operacion = this.operandoa - this.operandob;
+  }
+
+  onMultiplicar():void{
+    this.operacion = this.operandoa * this.operandob;
+  }
+
+  onDividir():void{
+    this.operacion = this.operandoa / this.operandob;
+  }
+
+  */
 }
